a7805da4165bba0dee28a7c47a666f3c8a410492
12DerekH <55858151+12DerekH@users.noreply.github.com>
2022-11-28 16:27:19 -0500

    Got TTS working

--- CustomTTS/app/src/main/java/com/example/customtts/ui/home/HomeFragment.java
+++ CustomTTS/app/src/main/java/com/example/customtts/ui/home/HomeFragment.java
@@ -1,11 +1,14 @@
 package com.example.customtts.ui.home;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,9 +17,14 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.customtts.R;
 import com.example.customtts.databinding.FragmentHomeBinding;
 
+import java.util.Locale;
+
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
+    TextToSpeech t1;
+    TextToSpeech t2;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,13 +34,56 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
         EditText ed1 =(EditText)root.findViewById(R.id.enterText);
-        System.out.println(ed1.getText());
+        Button b1 = (Button)root.findViewById(R.id.enterButton);
+
+        t1 = new TextToSpeech(root.getContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.addSpeech("Lorem", "");
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        t2 = new TextToSpeech(root.getContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t2.setLanguage(Locale.GERMAN);
+                }
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = ed1.getText().toString();
+                //Toast.makeText(root.getContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                for (Object s: t1.getVoices().toArray()) {
+                    System.out.println(s.toString());
+                }
+                //Toast.makeText(root.getContext(), t1.getVoices().toString(), Toast.LENGTH_LONG).show();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+                t2.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+                //System.out.println(ed1.getText());
+            }
+        });
 
         return root;
+    }
+
+    public void onPause(){
+        if(t1 !=null){
+            t1.stop();
+            //t1.shutdown();
+        }
+        if(t2 !=null){
+            t2.stop();
+            //t1.shutdown();
+        }
+        super.onPause();
     }
 
     @Override
